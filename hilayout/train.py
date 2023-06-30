0c91a9957233ef1a3effbaa30959fbd9b8970cf7
Nilesh Chakraborty <Nilesh.Chakraborty@iais.fraunhofer.de>
2023-06-30 13:10:58 +0200

    Mix json2token logic and train/test pipeline from donut + hierarchical layout T5 model

---
+++ hilayout/train.py
@@ -0,0 +1,158 @@
+"""
+Donut
+Copyright (c) 2022-present NAVER Corp.
+MIT License
+"""
+import argparse
+import datetime
+import json
+import os
+import random
+from io import BytesIO
+from os.path import basename
+from pathlib import Path
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.profilers import AdvancedProfiler
+from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
+from pytorch_lightning.plugins import CheckpointIO
+from pytorch_lightning.utilities import rank_zero_only
+from sconf import Config
+from hilayout.lightning_module import LayoutTransformerModelPLModule, LayoutTransformerDataPLModule
+from hilayout.util import LayoutTransformerDataset
+
+
+class CustomCheckpointIO(CheckpointIO):
+    def save_checkpoint(self, checkpoint, path, storage_options=None):
+        del checkpoint["state_dict"]
+        torch.save(checkpoint, path)
+
+    def load_checkpoint(self, path, storage_options=None):
+        checkpoint = torch.load(Path(path) / "artifacts.ckpt")
+        return checkpoint
+
+    def remove_checkpoint(self, path) -> None:
+        return super().remove_checkpoint(path)
+
+
+@rank_zero_only
+def save_config_file(config, path):
+    if not Path(path).exists():
+        os.makedirs(path)
+    save_path = Path(path) / "config.yaml"
+    print(config.dumps())
+    with open(save_path, "w") as f:
+        f.write(config.dumps(modified_color=None, quote_str=True))
+        print(f"Config is saved at {save_path}")
+
+
+def train(config):
+    torch.set_float32_matmul_precision("medium")
+    pl.seed_everything(config.get("seed", 42), workers=True)
+
+    # model_module = DonutModelPLModule.load_from_checkpoint(Path(config.result_path) / config.exp_name / config.exp_version/ "artifacts-v8.ckpt")
+    if config.load_from_checkpoint:
+        model_module = LayoutTransformerModelPLModule.load_from_checkpoint(Path(config.pretrained_model_name_or_path) / "artifacts.ckpt", config=config)
+    else:
+        model_module = LayoutTransformerModelPLModule(config)
+    data_module = LayoutTransformerDataPLModule(config)
+
+    # add datasets to data_module
+    datasets = {"train": [], "val": []}
+    for i, dataset_name_or_path in enumerate(config.dataset_name_or_paths):
+        task_name = os.path.basename(dataset_name_or_path)  # e.g., cord-v2, docvqa, rvlcdip, ...
+        
+        # add categorical special tokens (optional)
+        if task_name == "rvlcdip":
+            model_module.model.decoder.add_special_tokens([
+                "<advertisement/>", "<budget/>", "<email/>", "<file_folder/>", 
+                "<form/>", "<handwritten/>", "<invoice/>", "<letter/>", 
+                "<memo/>", "<news_article/>", "<presentation/>", "<questionnaire/>", 
+                "<resume/>", "<scientific_publication/>", "<scientific_report/>", "<specification/>"
+            ])
+        if task_name == "docvqa":
+            model_module.model.decoder.add_special_tokens(["<yes/>", "<no/>"])
+            
+        for split in ["train", "val"]:
+            datasets[split].append(
+                LayoutTransformerDataset(
+                    dataset_name_or_path=dataset_name_or_path,
+                    model=model_module.model,
+                    max_length=config.max_length,
+                    split=split,
+                    task_start_token=config.task_start_tokens[i]
+                    if config.get("task_start_tokens", None)
+                    else f"<s_{task_name}>",
+                    prompt_end_token="<s_answer>" if "docvqa" in dataset_name_or_path else f"<s_{task_name}>",
+                    sort_json_key=config.sort_json_key,
+                    page_tokens=config.page_tokens,
+                    max_pages=config.max_pages
+                )
+            )
+            # prompt_end_token is used for ignoring a given prompt in a loss function
+            # for docvqa task, i.e., {"question": {used as a prompt}, "answer": {prediction target}},
+            # set prompt_end_token to "<s_answer>"
+    data_module.train_datasets = datasets["train"]
+    data_module.val_datasets = datasets["val"]
+
+    logger = TensorBoardLogger(
+        save_dir=config.result_path,
+        name=config.exp_name,
+        version=config.exp_version,
+        default_hp_metric=False,
+    )
+
+    lr_callback = LearningRateMonitor(logging_interval="step")
+
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_metric",
+        dirpath=Path(config.result_path) / config.exp_name / config.exp_version,
+        filename="artifacts",
+        save_top_k=1,
+        save_last=True,
+        mode="min",
+    )
+
+    custom_ckpt = CustomCheckpointIO()
+    profiler = AdvancedProfiler(dirpath=".", filename="profiler.log")
+    trainer = pl.Trainer(
+        enable_checkpointing=config.get("enable_checkpointing", None),
+        num_nodes=config.get("num_nodes", 1),
+        # gpus=torch.cuda.device_count(),
+        # strategy="ddp",
+        profiler=profiler,
+        overfit_batches=config.get("overfit_batches", 0.0),
+        accelerator="gpu",
+        plugins=custom_ckpt,
+        max_epochs=config.max_epochs,
+        max_steps=config.max_steps,
+        val_check_interval=config.get("val_check_interval", 1.0),
+        check_val_every_n_epoch=config.get("check_val_every_n_epoch", 1),
+        gradient_clip_val=config.gradient_clip_val,
+        precision=16,
+        num_sanity_val_steps=0,
+        logger=logger,
+        callbacks=[lr_callback, checkpoint_callback],
+    )
+
+    trainer.fit(model_module, data_module)
+
+
+if __name__ == "__main__":
+    # os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, required=True)
+    parser.add_argument("--exp_version", type=str, required=False)
+    args, left_argv = parser.parse_known_args()
+    
+    config = Config(args.config)
+    config.argv_update(left_argv)
+
+    config.exp_name = basename(args.config).split(".")[0]
+    config.exp_version = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") if not args.exp_version else args.exp_version
+
+    save_config_file(config, Path(config.result_path) / config.exp_name / config.exp_version)
+    train(config)
---
+++ hilayout/util.py
@@ -0,0 +1,496 @@
+"""
+Donut
+Copyright (c) 2022-present NAVER Corp.
+MIT License
+"""
+import json
+import jsonlines
+import pickle
+import os
+import random
+from collections import defaultdict
+from typing import Any, Dict, List, Tuple, Union
+import numpy as np
+
+import torch
+from tqdm import tqdm
+import yaml
+import zss
+from datasets import load_dataset
+# from nltk import edit_distance
+from rapidfuzz.distance.Levenshtein import distance as lv_distance
+from torch.utils.data import Dataset
+from transformers.modeling_utils import PreTrainedModel
+from zss import Node
+from PIL import Image
+
+
+def save_json(write_path: Union[str, bytes, os.PathLike], save_obj: Any):
+    with open(write_path, "w") as f:
+        json.dump(save_obj, f)
+
+def save_yaml(path, data):
+    with open(path, 'w+') as f:
+        yaml.dump(data, f)
+
+def load_json(json_path: Union[str, bytes, os.PathLike]):
+    with open(json_path, "r") as f:
+        return json.load(f)
+
+def normalize_bbox(bbox, image_size):
+    width, height = image_size
+    x0, y0, x1, y1 = bbox
+    return [
+        np.clip(int(1000 * x0 / width), 0, 1000),
+        np.clip(int(1000 * y0 / height), 0, 1000),
+        np.clip(int(1000 * x1 / width), 0, 1000),
+        np.clip(int(1000 * y1 / height),  0, 1000)
+    ]
+
+def unnormalize_bbox(bbox, image_size):
+    width, height = image_size
+    x0, y0, x1, y1 = bbox
+    return [
+        (x0 * width) / 1000,
+        (y0 * height) / 1000,
+        (x1 * width) / 1000,
+        (y1 * height) / 1000
+    ]
+
+class LayoutTransformerDataset(Dataset):
+    """
+    DonutDataset which is saved in huggingface datasets format. (see details in https://huggingface.co/docs/datasets)
+    Each row, consists of image path(png/jpg/jpeg) and gt data (json/jsonl/txt),
+    and it will be converted into input_tensor(vectorized image) and input_ids(tokenized string)
+
+    Args:
+        dataset_name_or_path: name of dataset (available at huggingface.co/datasets) or the path containing image files and metadata.jsonl
+        ignore_id: ignore_index for torch.nn.CrossEntropyLoss
+        task_start_token: the special token to be fed to the decoder to conduct the target task
+    """
+
+    num_pages = 10
+
+    def __init__(
+        self,
+        dataset_name_or_path: str,
+        model: PreTrainedModel,
+        max_length: int,
+        split: str = "train",
+        ignore_id: int = -100,
+        task_start_token: str = "<s>",
+        prompt_end_token: str = None,
+        sort_json_key: bool = True,
+        page_tokens: int = 10,
+        max_pages: int = 10,
+        use_images: bool = False,
+        use_cache: bool = True
+    ):
+        super().__init__()
+
+        self.model = model
+        self.max_length = max_length
+        self.split = split
+        self.ignore_id = ignore_id
+        self.task_start_token = task_start_token
+        self.prompt_end_token = (
+            prompt_end_token if prompt_end_token else task_start_token
+        )
+        self.sort_json_key = sort_json_key
+        self.dataset_name_or_path = dataset_name_or_path
+        self.page_tokens = page_tokens
+        self.max_pages = max_pages
+        self.use_images = use_images
+        self.use_cache = use_cache
+
+        print(dataset_name_or_path, self.split)
+        self.dataset = self.load_dataset_multipage(
+            dataset_name_or_path, split=self.split
+        )
+        self.dataset_length = len(self.dataset)
+        print("Size of dataset split {}: {}".format(self.split, self.dataset_length))
+
+        self._generate_token_sequences()
+
+        self.model.add_special_tokens(
+            [self.task_start_token, self.prompt_end_token]
+        )
+        self.prompt_end_token_id = (
+            self.model.tokenizer.convert_tokens_to_ids(
+                self.prompt_end_token
+            )
+        )
+
+    def _generate_token_sequences(self):
+        # pickle gt_token_sequences and tokenizer additional_special_tokens, so that we don't have to recompute them every time
+        cache_path = os.path.join(self.dataset_name_or_path, f"cache_gt_tokens_{self.split}.pkl")
+        if self.use_cache and os.path.exists(cache_path):
+            print(f"Loading tokenized gt parses from cache at {cache_path}")
+            with open(cache_path, "rb") as f:
+                self.gt_token_sequences, additional_special_tokens = pickle.load(f)
+                self.model.add_special_tokens(additional_special_tokens)
+        else:
+            self.gt_token_sequences = []
+            for sample in tqdm(self.dataset, desc="Converting json parses to token sequences for split {}".format(self.split), dynamic_ncols=True, position=0, unit="sample"):
+                ground_truth = sample["ground_truth"]
+
+                if (
+                    "gt_parses" in ground_truth
+                ):  # when multiple ground truths are available, e.g., docvqa
+                    assert isinstance(ground_truth["gt_parses"], list)
+                    gt_jsons = ground_truth["gt_parses"]
+                else:
+                    assert "gt_parse" in ground_truth and isinstance(
+                        ground_truth["gt_parse"], dict
+                    )
+                    gt_jsons = [ground_truth["gt_parse"]]
+
+                self.gt_token_sequences.append(
+                    [
+                        self.task_start_token
+                        + self.model.json2token(
+                            gt_json,
+                            update_special_tokens_for_json_key=self.split
+                            == "train",
+                            sort_json_key=self.sort_json_key,
+                        )
+                        + self.model.tokenizer.eos_token
+                        for gt_json in gt_jsons  # load json from list of json
+                    ]
+                )
+                
+            if self.use_cache:
+                # save gt_token_sequences and tokenizer additional_special_tokens to cache
+                cache_path = os.path.join(self.dataset_name_or_path, f"cache_gt_tokens_{self.split}.pkl")
+                print(f"Saving tokenized gt parses to cache at {cache_path}")
+                with open(cache_path, "wb") as f:
+                    additional_special_tokens = self.model.tokenizer.additional_special_tokens
+                    pickle.dump((self.gt_token_sequences, additional_special_tokens), f)
+                
+        
+
+
+    def __len__(self) -> int:
+        return self.dataset_length
+    
+    def _get_boxes(self, tokens, page_size):
+        boxes = [(token["x0"], token["y0"], token["x1"], token["y1"]) for token in tokens]
+        return [normalize_bbox(box, page_size) for box in boxes]
+
+    def __getitem__(
+        self, idx: int
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        """
+        Load image from image_path of given dataset_path and convert into input_tensor and labels.
+        Convert gt data into input_ids (tokenized string)
+
+        Returns:
+            input_tensor : preprocessed image
+            input_ids : tokenized gt_data
+            labels : masked labels (model doesn't need to predict prompt and pad token)
+        """
+        sample = self.dataset[idx]
+        num_pages = len(sample["page_names"])
+        doc_name = sample["doc_name"]
+        page_path = os.path.join(self.dataset_name_or_path, "images")
+        page_size = sample["page_size"]
+        page_size = page_size["width"], page_size["height"]
+
+        words = []
+        boxes = []
+        context = []
+        image_paths = []
+
+        for page_ix in range(min(num_pages, self.max_pages)):
+            words.append([token["token_text"] for token in sample['tokens'][page_ix]])
+            boxes.append(np.array(self._get_boxes(sample['tokens'][page_ix], page_size), dtype=np.float32))
+            context.append(' '.join([token["token_text"] for token in sample['tokens'][page_ix]]))
+            image_paths.append(os.path.join(page_path, f"{doc_name}_page_{page_ix+1}.png"))
+
+        input_text = ["{:s}: context: {:s}".format("[PAGE]" * self.page_tokens, c) for c in context]
+        tokens = self.model.tokenizer(input_text, return_tensors='pt', padding=True, truncation=True, max_length=self.max_length)
+        input_ids = tokens.input_ids
+        attention_mask = tokens.attention_mask
+    
+        context_page_corresp = None
+
+        if num_pages < self.max_pages:
+            for _ in range(self.max_pages - num_pages):
+                words.append([''])
+                boxes.append(np.zeros([1, 4], dtype=np.float32))
+
+        if self.use_images:
+            images = [Image.open(img_path).convert("RGB") for img_path in image_paths]
+            images += [Image.new('RGB', (2, 2)) for i in range(self.max_pages - len(image_paths))]  # Pad with 2x2 images.
+
+        # input_ids
+        try:
+            processed_parse = random.choice(
+                self.gt_token_sequences[idx]
+            )  # can be more than one, e.g., DocVQA Task 1
+        except Exception as a:
+            print(self.gt_token_sequences[idx])
+            raise a
+        labels = self.model.tokenizer(
+            processed_parse,
+            add_special_tokens=False,
+            max_length=self.max_length,
+            padding="max_length",
+            truncation=True,
+            return_tensors="pt",
+        )["input_ids"].squeeze(0)
+        
+        prompt_end_index = None
+
+        # if self.split == "train":
+        #     labels[
+        #         labels == self.model.tokenizer.pad_token_id
+        #     ] = self.ignore_id  # model doesn't need to predict pad token
+        #     labels[
+        #         : torch.nonzero(labels == self.prompt_end_token_id).sum() + 1
+        #     ] = self.ignore_id  # model doesn't need to predict prompt (for VQA)
+        # else:
+        #     prompt_end_index = torch.nonzero(
+        #         labels == self.prompt_end_token_id
+        #     ).sum()  # return prompt end index instead of target output labels
+            
+        sample_dict = {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
+            "words": words,
+            "boxes": boxes,
+            "context": context,
+            "num_pages": num_pages,
+            "processed_parse": processed_parse,
+            "labels": labels
+        }
+        
+        return sample_dict
+        
+        
+    @classmethod
+    def load_dataset_multipage(cls, dataset_name_or_path, split):
+        import itertools
+        json_path = os.path.join(dataset_name_or_path, split, "metadata.jsonl")
+        
+        with jsonlines.open(json_path) as reader:
+            # return [data for data in itertools.islice(reader, 8)]
+            return [data for data in reader]
+                
+
+
+class JSONParseEvaluator:
+    """
+    Calculate n-TED(Normalized Tree Edit Distance) based accuracy and F1 accuracy score
+    """
+
+    @staticmethod
+    def flatten(data: dict):
+        """
+        Convert Dictionary into Non-nested Dictionary
+        Example:
+            input(dict)
+                {
+                    "menu": [
+                        {"name" : ["cake"], "count" : ["2"]},
+                        {"name" : ["juice"], "count" : ["1"]},
+                    ]
+                }
+            output(list)
+                [
+                    ("menu.name", "cake"),
+                    ("menu.count", "2"),
+                    ("menu.name", "juice"),
+                    ("menu.count", "1"),
+                ]
+        """
+        flatten_data = list()
+
+        def _flatten(value, key=""):
+            if type(value) is dict:
+                for child_key, child_value in value.items():
+                    _flatten(
+                        child_value, f"{key}.{child_key}" if key else child_key
+                    )
+            elif type(value) is list:
+                for value_item in value:
+                    _flatten(value_item, key)
+            else:
+                flatten_data.append((key, value))
+
+        _flatten(data)
+        return flatten_data
+
+    @staticmethod
+    def update_cost(node1: Node, node2: Node):
+        """
+        Update cost for tree edit distance.
+        If both are leaf node, calculate string edit distance between two labels (special token '<leaf>' will be ignored).
+        If one of them is leaf node, cost is length of string in leaf node + 1.
+        If neither are leaf node, cost is 0 if label1 is same with label2 othewise 1
+        """
+        label1 = node1.label
+        label2 = node2.label
+        label1_leaf = "<leaf>" in label1
+        label2_leaf = "<leaf>" in label2
+        if label1_leaf == True and label2_leaf == True:
+            return lv_distance(
+                label1.replace("<leaf>", ""), label2.replace("<leaf>", "")
+            )
+        elif label1_leaf == False and label2_leaf == True:
+            return 1 + len(label2.replace("<leaf>", ""))
+        elif label1_leaf == True and label2_leaf == False:
+            return 1 + len(label1.replace("<leaf>", ""))
+        else:
+            return int(label1 != label2)
+
+    @staticmethod
+    def insert_and_remove_cost(node: Node):
+        """
+        Insert and remove cost for tree edit distance.
+        If leaf node, cost is length of label name.
+        Otherwise, 1
+        """
+        label = node.label
+        if "<leaf>" in label:
+            return len(label.replace("<leaf>", ""))
+        else:
+            return 1
+
+    def normalize_dict(self, data: Union[Dict, List, Any]):
+        """
+        Sort by value, while iterate over element if data is list
+        """
+        if not data:
+            return {}
+
+        if isinstance(data, dict):
+            new_data = dict()
+            for key in sorted(data.keys(), key=lambda k: (len(k), k)):
+                value = self.normalize_dict(data[key])
+                if value:
+                    if not isinstance(value, list):
+                        value = [value]
+                    new_data[key] = value
+
+        elif isinstance(data, list):
+            if all(isinstance(item, dict) for item in data):
+                new_data = []
+                for item in data:
+                    item = self.normalize_dict(item)
+                    if item:
+                        new_data.append(item)
+            else:
+                new_data = [
+                    str(item).strip()
+                    for item in data
+                    if type(item) in {str, int, float} and str(item).strip()
+                ]
+        else:
+            new_data = [str(data).strip()]
+
+        return new_data
+
+    def cal_f1(self, preds: List[dict], answers: List[dict]):
+        """
+        Calculate global F1 accuracy score (field-level, micro-averaged) by counting all true positives, false negatives and false positives
+        """
+        total_tp, total_fn_or_fp = 0, 0
+        for pred, answer in zip(preds, answers):
+            pred, answer = self.flatten(
+                self.normalize_dict(pred)
+            ), self.flatten(self.normalize_dict(answer))
+            for field in pred:
+                if field in answer:
+                    total_tp += 1
+                    answer.remove(field)
+                else:
+                    total_fn_or_fp += 1
+            total_fn_or_fp += len(answer)
+        return total_tp / (total_tp + total_fn_or_fp / 2)
+
+    def construct_tree_from_dict(
+        self, data: Union[Dict, List], node_name: str = None
+    ):
+        """
+        Convert Dictionary into Tree
+
+        Example:
+            input(dict)
+
+                {
+                    "menu": [
+                        {"name" : ["cake"], "count" : ["2"]},
+                        {"name" : ["juice"], "count" : ["1"]},
+                    ]
+                }
+
+            output(tree)
+                                     <root>
+                                       |
+                                     menu
+                                    /    \
+                             <subtree>  <subtree>
+                            /      |     |      \
+                         name    count  name    count
+                        /         |     |         \
+                  <leaf>cake  <leaf>2  <leaf>juice  <leaf>1
+         """
+        if node_name is None:
+            node_name = "<root>"
+
+        node = Node(node_name)
+
+        if isinstance(data, dict):
+            for key, value in data.items():
+                kid_node = self.construct_tree_from_dict(value, key)
+                node.addkid(kid_node)
+        elif isinstance(data, list):
+            if all(isinstance(item, dict) for item in data):
+                for item in data:
+                    kid_node = self.construct_tree_from_dict(
+                        item,
+                        "<subtree>",
+                    )
+                    node.addkid(kid_node)
+            else:
+                for item in data:
+                    node.addkid(Node(f"<leaf>{item}"))
+        else:
+            raise Exception(data, node_name)
+        return node
+
+    def cal_acc(self, pred: dict, answer: dict):
+        """
+        Calculate normalized tree edit distance(nTED) based accuracy.
+        1) Construct tree from dict,
+        2) Get tree distance with insert/remove/update cost,
+        3) Divide distance with GT tree size (i.e., nTED),
+        4) Calculate nTED based accuracy. (= max(1 - nTED, 0 ).
+        """
+        pred = self.construct_tree_from_dict(self.normalize_dict(pred))
+        answer = self.construct_tree_from_dict(self.normalize_dict(answer))
+        return max(
+            0,
+            1
+            - (
+                zss.distance(
+                    pred,
+                    answer,
+                    get_children=zss.Node.get_children,
+                    insert_cost=self.insert_and_remove_cost,
+                    remove_cost=self.insert_and_remove_cost,
+                    update_cost=self.update_cost,
+                    return_operations=False,
+                )
+                / zss.distance(
+                    self.construct_tree_from_dict(self.normalize_dict({})),
+                    answer,
+                    get_children=zss.Node.get_children,
+                    insert_cost=self.insert_and_remove_cost,
+                    remove_cost=self.insert_and_remove_cost,
+                    update_cost=self.update_cost,
+                    return_operations=False,
+                )
+            ),
+        )
